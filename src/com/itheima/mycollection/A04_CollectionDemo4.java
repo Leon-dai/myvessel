588410049ce448dc66bf570ee3aaca4967a7a969
Leon <212649354@qq.com>
2024-02-26 18:40:20 +0800

    增强for遍历

---
+++ src/com/itheima/mycollection/A04_CollectionDemo4.java
@@ -0,0 +1,25 @@
+package com.itheima.mycollection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class A04_CollectionDemo4 {
+    public static void main(String[] args) {
+        //1.创建集合并添加元素
+        Collection<String> coll = new ArrayList<>();
+        coll.add("zhangsan");
+        coll.add("lisi");
+        coll.add("wangwu");
+
+        //2.利用增强for进行遍历
+        //快捷键  集合名称.for
+        for (String s : coll) {
+            s = "qqq";
+        }
+
+        for (String s : coll) {
+            System.out.println(s);
+        }
+    }
+}
